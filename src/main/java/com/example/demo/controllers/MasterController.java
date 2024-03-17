7b6f950a3c16843bef7ee2bed32929576bd0e683
Bruh <nazarenko-zhora@mail.ru>
2024-03-17 19:08:14 +0700

    Added new mappings in contoller, added html for Test,Test1,Test2, Test3 page, divided header to separate html block

--- src/main/java/com/example/demo/controllers/MasterController.java
+++ src/main/java/com/example/demo/controllers/MasterController.java
@@ -11,4 +11,28 @@ public class MasterController {
         model.addAttribute("title", "Main page");
         return "home";
     }
+
+    @GetMapping("/Test")
+    public String test(Model model) {
+        model.addAttribute("title", "Test page");
+        return "Test";
+    }
+
+    @GetMapping("/Test1")
+    public String test1(Model model) {
+        model.addAttribute("title", "Test1 page");
+        return "Test1";
+    }
+    @GetMapping("/Test2")
+    public String test2(Model model) {
+        model.addAttribute("title", "Test2 page");
+        return "Test2";
+    }
+    @GetMapping("/Test3")
+    public String test3(Model model) {
+        model.addAttribute("title", "Test3 page");
+        return "Test3";
+    }
+
+
 }
